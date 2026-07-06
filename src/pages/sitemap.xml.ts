572cff84adb97bb45ac42be2aa6a5e9caa6d35f5
Minjie DING <97084018+MinjieDING@users.noreply.github.com>
2026-07-06 21:49:31 +0800

    Add SEO basics for indexing

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,19 @@
+const pages = ['', '/project-courier-survey-summary'];
+
+export function GET() {
+	const urls = pages
+		.map((path) => {
+			const href = new URL(path || '/', 'https://minjied.com').toString();
+			return `<url><loc>${href}</loc></url>`;
+		})
+		.join('');
+
+	const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
+
+	return new Response(xml, {
+		headers: {
+			'Content-Type': 'application/xml; charset=utf-8',
+		},
+	});
+}
